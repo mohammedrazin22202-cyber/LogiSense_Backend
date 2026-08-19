ef187e48ff5ba65f45961099f19eeaa673da4d5b
mohammedrazin22202-cyber <mohammedrazin22202@gmail.com>
2026-08-19 09:14:02 +0530

    style: align console message formatting in main seeder

--- seed.py
+++ seed.py
@@ -293,3 +293,5 @@ if __name__ == '__main__':
 # Commit tweak 7: docs: update comments on orders_master coordinates
 
 # Commit tweak 17: style: adjust seeding progress output formatting
+
+# Commit tweak 27: style: align console message formatting in main seeder
